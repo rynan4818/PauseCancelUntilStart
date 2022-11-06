3137ac0d51e9590c4d686c171b4c1c4b0de10c7a
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2022-11-06 13:54:17 +0900

    AutoPauseStealthの停止中はメニューボタンをキャンセル

---
+++ PauseCancelUntilStart/HarmonyPatches/MenuButtonDownPatch.cs
@@ -0,0 +1,16 @@
+﻿using AutoPauseStealth;
+using HarmonyLib;
+
+namespace PauseCancelUntilStart.HarmonyPatches
+{
+    [HarmonyPatch(typeof(VRControllersInputManager))]
+    [HarmonyPatch("MenuButtonDown", MethodType.Normal)]
+    public class MenuButtonDownPatch
+    {
+        static void Postfix(ref bool __result)
+        {
+            if (AutoPauseStealthController.StabilityPeriodActive)
+                __result = false;
+        }
+    }
+}
--- PauseCancelUntilStart/Plugin.cs
+++ PauseCancelUntilStart/Plugin.cs
@@ -1,4 +1,6 @@
-﻿using IPA;
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
 using IPALogger = IPA.Logging.Logger;
 
 namespace PauseCancelUntilStart
@@ -11,24 +13,30 @@ namespace PauseCancelUntilStart
         /// Use to send log messages through BSIPA.
         /// </summary>
         internal static IPALogger Log { get; private set; }
+        private Harmony _harmony;
+        public const string HARMONY_ID = "com.github.rynan4818.PauseCancelUntilStart";
 
         [Init]
         public Plugin(IPALogger logger)
         {
             Instance = this;
             Log = logger;
+            Log.Debug("Initialized.");
+            this._harmony = new Harmony(HARMONY_ID);
         }
 
         [OnStart]
         public void OnApplicationStart()
         {
             Plugin.Log.Info("OnApplicationStart");
+            this._harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         [OnExit]
         public void OnApplicationQuit()
         {
-
+            Log.Debug("OnApplicationQuit");
+            this._harmony.UnpatchSelf();
         }
 
     }
